cc851298fc267b3614579d2092a54da61f0c8f3e
Andrew Vandermeer <andrew.vandermeer@mbp-1shtdf.bs.local>
2018-08-29 15:20:12 -0600

    Added another graph

--- graph.js
+++ graph.js
@@ -19,7 +19,58 @@ let myGraph = new Graph();
 myGraph.addVertex('asdf');
 myGraph.addVertex('qwer');
 myGraph.addVertex('zxcv');
-
 myGraph.addEdge('asdf', 'zxcv');
 myGraph.addEdge('asdf', 'qwer');
 console.log(myGraph.vertices);
+
+function Graph2() {
+  this.storage = {};
+  this.size = 0;
+}
+
+Graph2.prototype.add = function(value) {
+  this.storage[value] = value;
+  this.size++;
+}
+
+Graph2.prototype.addConnection = function(from, to) {
+  this.storage[from][to] = true;
+  this.storage[to][from] = true;
+}
+
+Graph2.prototype.removeConnection = function(from, to) {
+  delete this.storage[from][to];
+  delete this.storage[to][from];
+}
+
+Graph2.prototype.contains = function(target) {
+  return this.storage.hasOwnProperty(target);
+}
+
+Graph2.prototype.hasConnection = function(val) {
+  return this.storage[from].hasOwnProperty[to];
+}
+
+Graph2.prototype.remove = function(val) {
+  delete this.storage[val];
+  this.size--;
+  for(let key in this.storage) {
+    if(this.storage[key][val]) {
+      delete this.storage[key][val];
+    }
+  }
+}
+
+let con = new Graph2();
+con.add('Sony');
+con.add('PS3');
+con.add('PS4');
+con.add('VR');
+con.add('Nintendo');
+con.add('Wii');
+con.add('Wii U');
+con.addConnection('Sony', 'PS3');
+con.addConnection('Sony', 'PS4');
+con.addConnection('Sony', 'VR');
+con.addConnection('Nintendo', 'Wii');
+con.addConnection('Nintendo', 'Wii U');
